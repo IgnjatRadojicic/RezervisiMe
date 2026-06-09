04edfe61d2232bb78f1edc18ca3e59df2b6f04e4
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-06-09 21:42:01 +0200

    feat: add user service interface

---
+++ RezervisiMe/RezervisiMe.API/Services/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using RezervisiMe.RezervisiMe.API.Infrastructure;
+using RezervisiMe.RezervisiMe.API.Models.Dto;
+using RezervisiMe.RezervisiMe.API.Models.Requests;
+
+namespace RezervisiMe.RezervisiMe.API.Services
+{
+    public interface IUserService
+    {
+        Result<UserDto> Register(RegisterRequest req, bool createdByAdmin);
+        Result<UserDto> Login(string username, string password);
+        Result<UserDto> GetById(Guid userId);
+        Result<UserDto> UpdateProfile(Guid userId, UpdateProfileRequest req);
+        Result DeleteUser(Guid userId);
+        IEnumerable<UserDto> Search(UserSearchCriteria criteria);
+    }
+}
