49eaea508a6c896b66cbf4303bc0c371a6efc3a1
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-06-09 21:21:28 +0200

    feat: add result patern handling

--- RezervisiMe/RezervisiMe.API/Infrastructure/Result.cs
+++ RezervisiMe/RezervisiMe.API/Infrastructure/Result.cs
@@ -25,4 +25,18 @@ namespace RezervisiMe.RezervisiMe.API.Infrastructure
         public static implicit operator Result<T>(T value) => Success(value);
         public static implicit operator Result<T>(Error error) => Failure(error);
     }
+        public class Result
+        {
+            public Error Error { get; }
+            public bool IsSuccess { get; }
+            public bool IsFailure => !IsSuccess;
+
+            private Result() { IsSuccess = true; }
+            private Result(Error error) { Error = error; IsSuccess = false; }
+
+            public static Result Success() => new Result();
+            public static Result Failure(Error error) => new Result(error);
+
+            public static implicit operator Result(Error error) => Failure(error);
+        }
 }
