19a27f96dd11e680114a795f3f64d097b6aafab5
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-06-09 18:56:03 +0200

    feat: add Result pattern

---
+++ RezervisiMe/RezervisiMe.API/Infrastructure/Error.cs
@@ -0,0 +1,21 @@
+﻿
+namespace RezervisiMe.RezervisiMe.API.Infrastructure
+{
+    public class Error
+    {
+        public string Code { get; }
+        public string Description { get; }
+
+        public Error(string code, string description)
+        {
+            Code = code ?? string.Empty;
+            Description = description ?? string.Empty;
+        }
+        public static Error NotFound(string description) => new Error("NotFound", description);
+        public static Error Validation(string description) => new Error("Validation", description);
+        public static Error Conflict(string description) => new Error("Conflict", description);
+        public static Error Forbidden(string description) => new Error("Forbidden", description);
+        public static Error Unauthorized(string description) => new Error("Unauthorized", description);
+        public static Error Internal(string description) => new Error("Internal", description);
+    }
+}
---
+++ RezervisiMe/RezervisiMe.API/Infrastructure/Result.cs
@@ -0,0 +1,28 @@
+﻿
+namespace RezervisiMe.RezervisiMe.API.Infrastructure
+{
+    public class Result<T>
+    {
+        public T Value { get; }
+        public Error Error { get; }
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+
+        private Result(T Value)
+        {
+            Value = Value;
+            IsSuccess = true;
+        }
+        private Result(Error error)
+        {
+            Error = error;
+            IsSuccess = false;
+        }
+
+        public static Result<T> Success(T Value) => new Result<T>(Value);
+        public static Result<T> Failure(Error error) => new Result<T>(error);
+
+        public static implicit operator Result<T>(T value) => Success(value);
+        public static implicit operator Result<T>(Error error) => Failure(error);
+    }
+}
