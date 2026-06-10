b0430a7e28bbc9fe908a08fc5805f2417bb62728
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-06-10 19:48:55 +0200

    feat: add review requests

---
+++ RezervisiMe/RezervisiMe.API/Models/Requests/ReviewRequests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RezervisiMe.RezervisiMe.API.Models.Requests
+{
+    public class CreateReviewRequest
+    {
+        public Guid AccommodationId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public string ImagePath { get; set; }   // opciono
+    }
+
+    public class UpdateReviewRequest
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public string ImagePath { get; set; }   
+    }
+}
