2bd01f62263202acec63c60db7705350c0ab6ddd
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-06-10 19:45:08 +0200

    feat: add reservation requests

---
+++ RezervisiMe/RezervisiMe.API/Models/Requests/ReservationRequests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Newtonsoft.Json;
+using RezervisiMe.RezervisiMe.API.Infrastructure;
+
+namespace RezervisiMe.RezervisiMe.API.Models.Requests
+{
+    public class CreateReservationRequest
+    {
+        public Guid AccommodationId { get; set; }
+
+        [JsonConverter(typeof(CustomDateConverter))]
+        public DateTime CheckIn { get; set; }
+
+        [JsonConverter(typeof(CustomDateConverter))]
+        public DateTime CheckOut { get; set; }
+
+        public int NumberOfGuests { get; set; }
+    }
+}
