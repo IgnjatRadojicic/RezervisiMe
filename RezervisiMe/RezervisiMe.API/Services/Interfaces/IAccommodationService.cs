99eba2d06271b3a94a6ba358c5ee5cd3958f44c0
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-06-10 19:43:53 +0200

    feat: add accommodation interface

---
+++ RezervisiMe/RezervisiMe.API/Services/Interfaces/IAccommodationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using RezervisiMe.RezervisiMe.API.Infrastructure;
+using RezervisiMe.RezervisiMe.API.Models;
+using RezervisiMe.RezervisiMe.API.Models.Dto;
+using RezervisiMe.RezervisiMe.API.Models.Requests;
+
+namespace RezervisiMe.RezervisiMe.API.Services
+{
+    public interface IAccommodationService
+    {
+        Result<AccommodationDto> Create(Guid hostId, CreateAccommodationRequest req);
+        Result<AccommodationDto> Update(Guid id, UpdateAccommodationRequest req,
+            Guid currentUserId, UserRole currentUserRole);
+        Result Delete(Guid id, Guid currentUserId, UserRole currentUserRole);
+        Result<AccommodationDto> GetById(Guid id);
+        IEnumerable<AccommodationDto> Search(AccommodationSearchCriteria criteria);
+    }
+}
