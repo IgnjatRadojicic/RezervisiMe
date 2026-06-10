4a38734965b9ffb36a8b4f60fc8303c85739933c
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-06-10 19:47:39 +0200

    feat: add reservation interface

---
+++ RezervisiMe/RezervisiMe.API/Services/Interfaces/IReservationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using RezervisiMe.RezervisiMe.API.Infrastructure;
+using RezervisiMe.RezervisiMe.API.Models;
+using RezervisiMe.RezervisiMe.API.Models.Dto;
+using RezervisiMe.RezervisiMe.API.Models.Requests;
+
+namespace RezervisiMe.RezervisiMe.API.Services
+{
+    public interface IReservationService
+    {
+        Result<ReservationDto> Create(Guid guestId, CreateReservationRequest req);
+        Result<ReservationDto> Cancel(Guid reservationId, Guid currentUserId, UserRole currentUserRole);
+        Result<ReservationDto> Approve(Guid reservationId);
+        Result<ReservationDto> Reject(Guid reservationId);
+        Result<ReservationDto> GetById(Guid id);
+        IEnumerable<ReservationDto> GetByGuest(Guid guestId, ReservationStatus? statusFilter);
+        IEnumerable<ReservationDto> GetAll(ReservationStatus? statusFilter, Guid? accommodationIdFilter);
+        void RefreshCompletedStatuses();
+    }
+}
